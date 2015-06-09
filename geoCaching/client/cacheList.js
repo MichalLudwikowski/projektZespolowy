04f5638e5d36a29943825d93ad982785ff863a2d
Babrzy93 <babrzy93@gmail.com>
2015-06-09 16:08:23 +0200

    testy

--- geoCaching/client/cacheList.js
+++ geoCaching/client/cacheList.js
@@ -1,6 +1,17 @@
+var lat, lng;
+Template.cacheList.rendered = function () {
+    navigator.geolocation.getCurrentPosition(handleSync);
+};
+var handleSync = function (position) {
+    lat = position.coords.latitude;
+    lng = position.coords.longitude;
+};
 Template.cacheList.helpers({
     caches: function () {
+
         var cachy = Caches.find();
+
         return cachy;
     }
+
 });
