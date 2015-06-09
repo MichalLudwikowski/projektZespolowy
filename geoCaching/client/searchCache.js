0e3f1e1e9e8fab01b2e0adb186270169c844f720
Lukasz <banaszekl@wp.pl>
2015-06-09 16:07:49 +0200

    mam 500 gram

--- geoCaching/client/searchCache.js
+++ geoCaching/client/searchCache.js
@@ -1,21 +1,20 @@
-var lat, lng;
+var lat, lng, mark, distance;
 Meteor.startup(function () {
     GoogleMaps.load();
 });
-Template.searchCache.rendered = function () {
-    navigator.geolocation.getCurrentPosition(handleSync);
-};
 Template.searchCache.helpers({
     exampleMapOptions2: function () {
+        navigator.geolocation.getCurrentPosition(handleSync);
         // Make sure the maps API has loaded
+        mark = Caches.findOne(this._id);
         if (GoogleMaps.loaded()) {
             // Map initialization options
-            var mark = Caches.findOne(this._id);
-            console.log(mark);
+            console.log("tak2");
             return {
                 center: new google.maps.LatLng(mark.coord_x, mark.coord_y),
-                zoom: 8
+                zoom: 12
             };
+
         }
     }
 });
@@ -24,26 +23,42 @@ var handleSync = function (position) {
     lng = position.coords.longitude;
 };
 Template.searchCache.onCreated(function () {
-
+    navigator.geolocation.getCurrentPosition(handleSync);
     GoogleMaps.ready('exampleMap2', function (map) {
-
         // Add a marker to the map once it's rea
-
         var marker = new google.maps.Marker({
             position: map.options.center,
-            animation: google.maps.Animation.DROP,
             map: map.instance
         });
         setTimeout(function () {
-            if (lat != undefined) {
-
-                var playerMarker = new google.maps.Marker({
-                    position: new google.maps.LatLng(lat, lng),
-                    animation: google.maps.Animation.DROP,
-                    map: map.instance
-                });
-            }
+            var playerMarker = new google.maps.Marker({
+                position: new google.maps.LatLng(lat, lng),
+                map: map.instance
+            });
         }, 5000);
-
     });
+    Meteor.setInterval(function () {
+        navigator.geolocation.getCurrentPosition(handleSync);
+        GoogleMaps.ready('exampleMap2', function (map) {
+            var playerMarker = new google.maps.Marker({
+                position: new google.maps.LatLng(lat, lng),
+                map: map.instance
+            });
+            distance = getDistance(mark.coord_x, mark.coord_y, lat, lng);
+        });
+    }, 5000);
 });
+var rad = function (x) {
+    return x * Math.PI / 180;
+};
+var getDistance = function (p1, p2, p3, p4) {
+    var R = 6378137; // Earth’s mean radius in meter
+    var dLat = rad(p3 - p1);
+    var dLong = rad(p4 - p2);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(rad(p1)) * Math.cos(rad(p3)) *
+        Math.sin(dLong / 2) * Math.sin(dLong / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return d; // returns the distance in meter
+};
